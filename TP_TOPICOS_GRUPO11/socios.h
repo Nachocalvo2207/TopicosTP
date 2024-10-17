0dbaf37d008326b428521bdb636fdb430b71aab8
Nachocalvo2207 <nachocalvo22@gmail.com>
2024-10-16 23:38:54 -0300

    Carga de Structs
    Socios y t_fecha

--- TP_TOPICOS_GRUPO11/socios.h
+++ TP_TOPICOS_GRUPO11/socios.h
@@ -1,6 +1,23 @@
 #ifndef SOCIOS_H_INCLUDED
 #define SOCIOS_H_INCLUDED
 
+typedef struct {
+    unsigned long DNI;
+    char ApYNom[60];
+    t_fecha fechaNac;
+    char sexo [1];
+    t_fecha fechaAfiliacion;
+    char categoria [10];
+    t_fecha UltCuotaPaga;
+    char estado[1];
+    t_fecha fechaBaja;
+}Socio;
+
+typedef struct {
+    int dia;
+    int mes;
+    int anio;
+}t_fecha;
 
 
 #endif // SOCIOS_H_INCLUDED
