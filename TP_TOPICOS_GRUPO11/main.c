6e52c9d04833feb67a345dd5eae86eee0300015d
Iara :) <67611230+iarasolro@users.noreply.github.com>
2024-10-16 23:47:50 -0300

    s

--- TP_TOPICOS_GRUPO11/main.c
+++ TP_TOPICOS_GRUPO11/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "socios.h"
 //
 //// Integrantes del grupo:
 //    -----------------
---
+++ TP_TOPICOS_GRUPO11/socios.c
@@ -0,0 +1,5 @@
+#include "socios.h"
+
+int normalizar_a_y_n(Socio * soc){
+    soc->ApYNom
+}
